cdcc9e093f7a671042a0b152d8bdb7e78a4edd2c
xiaoshan40 <124234427+xiaoshan40@users.noreply.github.com>
2025-01-02 15:20:17 +0800

    Update combination_fasta.pl

--- scripts/combination_fasta.pl
+++ scripts/combination_fasta.pl
@@ -1,1 +1,30 @@
+#!/usr/bin/perl -w
+use strict;
+
+my @array_name = qw /AmyTra ArcPla BomMor DanPle EupSim GynQin HelArm LymMon ManSex OpeBru OrgAnt PluXyl SpoLit TriNi/ ;
+my %hash_a;
+
+opendir DIR, "group_trimal" or die "Can't open dir!";
+while (my $file = readdir DIR) {
+        next if $file=~/^\./;
+        open File, "group_trimal/".$file or die "Can't open file!";
+        my %hash;
+        my $id;
+        while (<File>) {
+                chomp();
+                if (/^>(\S*?)_/) {
+                        $id = $1;
+                } else {
+                        $hash{$id} .= $_;
+                }
+        }
+        foreach (@array_name) {
+                $hash_a{$_}.=$hash{$_};
+        }
+}
+closedir DIR;
+
+foreach (@array_name) {
+        print ">".$_."\n".$hash_a{$_}."\n";
+}
 
